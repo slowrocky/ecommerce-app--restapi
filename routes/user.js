b2709f6fadd7acca3471e9fac41ad22a4264b105
Matúš Holotňák <matus.holotnak@gmail.com>
2022-03-26 17:50:09 +0100

    complete routes get, put

--- routes/user.js
+++ routes/user.js
@@ -4,4 +4,31 @@ const UserService = require("../services/UserService");
 
 const UserServiceInstance = new UserService();
 
-module.exports = (app) => {};
+module.exports = (app) => {
+  app.use("/users", router);
+
+  router.get("/:userId", async (req, res, next) => {
+    try {
+      const { userId } = req.params;
+
+      const response = await UserServiceInstance.get({ id: userId });
+      res.status(200).send(response);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.put("/:userId", async (req, res, next) => {
+    try {
+      const { userId } = req.params;
+      const data = req.body;
+
+      const response = await UserServiceInstance.update({
+        id: userId,
+        ...data,
+      });
+    } catch (err) {
+      next(err);
+    }
+  });
+};

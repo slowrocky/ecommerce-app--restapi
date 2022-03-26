868333f65bbda0563df5a40b3d6fbb58bca46705
Matúš Holotňák <matus.holotnak@gmail.com>
2022-03-26 17:11:39 +0100

    add findOneByUser, findOneById

--- models/cart.js
+++ models/cart.js
@@ -1,9 +1,8 @@
-const db = require('../db');
-const moment = require('moment');
-const pgp = require('pg-promise')({ capSQL: true});
+const db = require("../db");
+const moment = require("moment");
+const pgp = require("pg-promise")({ capSQL: true });
 
 module.exports = class CartModel {
-
   constructor(data = {}) {
     this.created = data.created || moment.utc().toISOString();
     this.modified = moment.utc().toISOString();
@@ -11,13 +10,17 @@ module.exports = class CartModel {
     this.isActive = data.isActive || true;
   }
 
+   /**
+   * Creates a new cart for a user
+   * @param  {Object}      data [User data]
+   * @return {Object|null}      [Created user record]
+   */
   async create(userId) {
     try {
-      
-      const data = { userId, ...this}
+      const data = { userId, ...this };
 
       // Generate SQL statement - using helper for dynamic parameter injection
-      const statement = pgp.helpers.insert(data, null, 'carts') + 'RETURNING *';
+      const statement = pgp.helpers.insert(data, null, "carts") + "RETURNING *";
 
       // Execute SQL statement
       const result = await db.query(statement);
@@ -31,4 +34,56 @@ module.exports = class CartModel {
       throw new Error(err);
     }
   }
-}
+
+  /**
+   * Loads a cart by User ID
+   * @param  {number}      id [User ID]
+   * @return {Object|null}    [Cart record]
+   */
+  static async findOneByUser(userId) {
+    try {
+      
+      // Generate SQL statement
+      const statement = `SELECT * FROM carts WHERE "userId" = $1`;
+      const values = [userId];
+
+      // Execute SQL statement
+      const result = await db.query(statement,values);
+
+      if (result.rows?.length) {
+        return result.rows[0];
+      }
+
+      return null;
+
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+
+  /**
+   * Loads a cart by ID
+   * @param  {number}      id [Cart ID]
+   * @return {Object|null}    [Cart record]
+   */
+  static async findOneById(id) {
+    try {
+      
+      // Generate SQL statement
+      const statement = `SELECT * FROM carts WHERE "id" = $1`;
+      const values = [id];
+
+      //Execute SQL statement
+      const result = await db.query(statement, values);
+
+      if (result.rows?.length) {
+        return result.rows[0];
+      }
+
+      return null;
+    } catch (err) {
+      throw new Error(err);
+    }
+  }
+
+};

f3d46ed66a7001dfe56137c27047bf995a008fae
Matúš Holotňák <matus.holotnak@gmail.com>
2022-03-26 17:44:49 +0100

    complete get, update

--- services/UserService.js
+++ services/UserService.js
@@ -3,6 +3,36 @@ const UserModel = require("../models/user");
 const UserModelInstance = new UserModel();
 
 module.exports = class UserService {
-  async get(data) {}
-  async update(data) {}
+  async get(data) {
+    const { id } = data;
+
+    try {
+      // Check if user already exists
+      const user = await UserModelInstance.findOneById(id);
+
+      // If user doesn't exist, reject
+      if (!user) {
+        throw createError(404, "User record not found");
+      }
+
+      return user;
+    } catch (err) {
+      throw err;
+    }
+  }
+  async update(data) {
+    try {
+      // Check if user already exists
+      const user = await UserModelInstance.update(data);
+
+      // If user doesn't exist, reject
+      if (!user) {
+        throw createError(404, "User record not found");
+      }
+
+      return user;
+    } catch (err) {
+      throw err;
+    }
+  }
 };

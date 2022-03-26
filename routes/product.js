fe72c1da7aa14e5731cc62dec9f7bd706d9593ea
Matúš Holotňák <matus.holotnak@gmail.com>
2022-03-26 18:05:26 +0100

    complete get for /,/:id

--- routes/product.js
+++ routes/product.js
@@ -4,4 +4,28 @@ const router = express.Router();
 const ProductService = require("../services/ProductService");
 const ProductServiceInstance = new ProductService();
 
-module.exports = (app) => {};
+module.exports = (app) => {
+  app.use("/products", router);
+
+  router.get("/", async (req, res, next) => {
+    try {
+      const queryParams = req.query;
+
+      const response = await ProductServiceInstance.list(queryParams);
+      res.status(200).send(response);
+    } catch (err) {
+      next(err);
+    }
+  });
+
+  router.get("/:productId", async (req, res, next) => {
+    try {
+      const { productId } = req.params;
+      const response = await ProductServiceInstance.get(productId);
+
+      res.status(200).send(response);
+    } catch (err) {
+      next(err);
+    }
+  });
+};
